2078833e836e6300b1cef388d9c4bbf511001e8f
Tommy MARCHAL <70965412+MarchalTommy@users.noreply.github.com>
2024-11-05 16:21:43 +0100

    initial modular commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.atoute"
+    namespace = "com.martodev.atoute"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.atoute"
+        applicationId = "com.martodev.atoute"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
